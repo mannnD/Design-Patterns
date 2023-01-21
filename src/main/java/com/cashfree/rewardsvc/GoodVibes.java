4e0fd9730bcc1d183e0ac65848214ce529318ec1
Manan Dubey <manandubey@CFs-MBP-196.bbrouter>
2023-01-21 18:29:33 +0530

    initiated

---
+++ src/main/java/com/cashfree/rewardsvc/GoodVibes.java
@@ -0,0 +1,13 @@
+package com.cashfree.rewardsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodVibes {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodVibes.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashfree/rewardsvc/RewardsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashfree.rewardsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
