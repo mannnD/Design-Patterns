0f58727bffcf5cec1dd42c42b1b138b8442160e9
Manan Dubey <manandubey@INKACTXM537.local>
2024-09-14 19:34:03 +0530

    added product and utils

---
+++ rewardsvc/src/main/java/com/cashfree/rewardsvc/Product.java
@@ -0,0 +1,7 @@
+package rewardsvc/src/main/java/com/cashfree/rewardsvc;
+
+public enum Product {
+    AP_INDIA, EInvoice, GST, CLEARINV, MaxItc, ClearCapture, EINVOICE, MAX_ITC, EINVOICE_INDIA, EINVOICE_GCC,
+    EINVOICE_MY, EINVOICE_XML_MY;
+
+}
